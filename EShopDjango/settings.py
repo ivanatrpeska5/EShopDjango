84956162ba320a8449e8c129794d48894aefaa04
ivana.trpeska <itrpeska5@gmail.com>
2023-07-01 21:06:46 +0200

    Fixes

--- EShopDjango/settings.py
+++ EShopDjango/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'photos/'
-MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT = BASE_DIR / 'photos'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
